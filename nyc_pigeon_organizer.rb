f647760fac64674d462ff266febf529fa2328826
Ian Hutson <ih00209@georgiasouthern.edu>
2020-02-10 16:32:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   hash = {}
   # data is original hash 
   data.each do |key, value|
-    
+    #key == ":color, :gender, or 'lives'
     value.each do |new_value, names|
       names.each do |name|
     print "______________"
