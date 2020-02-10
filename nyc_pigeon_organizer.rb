0e939458487ab5a169510bdd0ed28073e8011c2b
Ian Hutson <ih00209@georgiasouthern.edu>
2020-02-10 16:36:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,14 @@ def nyc_pigeon_organizer(data)
   hash = {}
   # data is original hash 
   data.each do |key, value|
-    #key == ":color, :gender, or 'lives'"
-    #value =
+    #key == ":color, :gender, or :lives"
+    #value == another key value pair i.e ":grey => ["Theo", "Peter Jr.", "Ms. K"]
     value.each do |new_value, names|
+      #new_value == line 6 key a.k.a previous key
+      #names == [...names]
       names.each do |name|
+        #name to key with values of key from line 5
+        #keys from line 11 will point to values from new_value8
     print "______________"
     print name
     print "______________"
